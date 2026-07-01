70803769508d9b3f21ae7496e38b5e31fdf81f8b
Jorge Martín <jorgem@element.io>
2026-07-01 18:16:20 +0200

    WIP

--- libraries/wellknown/api/src/main/kotlin/io/element/android/libraries/wellknown/api/WellknownRetrieverResult.kt
+++ libraries/wellknown/api/src/main/kotlin/io/element/android/libraries/wellknown/api/WellknownRetrieverResult.kt
@@ -15,6 +15,11 @@ sealed interface WellknownRetrieverResult<out T> {
     data class Success<out T>(val data: T) : WellknownRetrieverResult<T>
 
     /**
+     * Well-known data has been retrieved from the local cache but is outdated and a manual refresh has been launched.
+     */
+    data class Outdated<out T>(val data: T) : WellknownRetrieverResult<T>
+
+    /**
      * Well-known data is not found (file does not exist server side, we got a 404).
      */
     data object NotFound : WellknownRetrieverResult<Nothing>
