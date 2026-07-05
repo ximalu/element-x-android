236c26261dd71ba359847efcc01e01364825ccfc
Ubuntu <ubuntu@localhost.localdomain>
2026-07-05 20:50:25 +0800

    ExMod: use debug build (faster), ExMod naming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,13 +91,13 @@ android {
     buildTypes {
         val oAuthRedirectSchemeBase = BuildTimeConfig.METADATA_HOST_REVERSED ?: "io.element.android"
         getByName("debug") {
-            resValue("string", "app_name", "$baseAppName dbg")
+            resValue("string", "app_name", "ExMod")
             resValue(
                 "string",
                 "login_redirect_scheme",
                 "$oAuthRedirectSchemeBase.debug",
             )
-            applicationIdSuffix = ".debug"
+            applicationIdSuffix = ".exmod"
             signingConfig = signingConfigs.getByName("debug")
         }
 
